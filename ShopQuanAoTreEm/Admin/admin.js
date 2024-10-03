1a2a5943bd7dca9eb4c121f91e0555bbec37d7f1
LAPTOP-KF0OD6HC\ASUS <mybui11qwer@gmail.com>
2024-10-03 08:41:52 +0700

    Delete File VSCode, readme

--- ShopQuanAoTreEm/Admin/admin.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Add JavaScript interactivity here, like toggling menus, fetching data dynamically, etc.
-});
--- ShopQuanAoTreEm/User/Index.js
+++
@@ -1,5 +0,0 @@
-const BuyProduct = document.getElementById("Buy");
-BuyProduct.addEventListener('click', function(event) {
-    alert("Da them vao gio hang")
-})
-
