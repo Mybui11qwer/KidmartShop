f50cb32af1265eec4de69a85b4d75ed82869a92a
Mybui11qwer <mybui11qwer@gmail.com>
2025-02-19 09:50:37 +0700

    Reset Project

---
+++ KidMartStore/KidMartStore/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using KidMartStore.Models;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net.PeerToPeer;
+using System.Web.UI.WebControls;
+
+public class AccountController : Controller
+{
+    public KidMartStoreEntities db = new KidMartStoreEntities();
+
+    public ActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public ActionResult Login(Customer customer)
+    {
+        if (ModelState.IsValid)
+        {
+            // Kiểm tra thông tin đăng nhập
+            var checkUser = db.Customers.FirstOrDefault(u => u.Email == customer.Email && u.Password == customer.Password);
+            if (checkUser != null)
+            {
+                // Lưu tên người dùng vào session
+                Session["Email"] = checkUser.Email;
+                Session["Name"] = checkUser.Username;
+                Session["Address"] = checkUser.Address;
+                Session["Phone"] = checkUser.Phone;
+                Session["ID_Customer"] = checkUser.ID_Customer;
+                Session["Role"] = checkUser.Role;
+
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không chính xác.");
+        }
+        return View();
+    }
+    public ActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Register(Customer NewCustomer)
+    {
+        try
+        {
+            NewCustomer.Role = "Khách Hàng";
+            db.Customers.Add(NewCustomer);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+        }
+        catch
+        {
+            return View("Register");
+        }       
+    }
+
+    // Đăng xuất
+    public ActionResult Logout()
+    {
+        Session.Clear(); // Xóa tất cả session khi đăng xuất
+        return RedirectToAction("Login");
+    }
+}
